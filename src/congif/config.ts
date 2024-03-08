2b6c39a71e7675e13ed5a28bd659cc59023015df
Taha Ratlam Wala <56840043+taharatlam@users.noreply.github.com>
2024-03-08 15:54:12 +0530

    changes0

--- src/congif/config.ts
+++ src/congif/config.ts
@@ -1,6 +1,6 @@
 const config = {
-    API_BASE_URL: 'http://93.188.164.69/api',
-    API_DOC_URL: 'http://93.188.164.69',
+    API_BASE_URL: 'http://93.188.164.69:8081/api',
+    API_DOC_URL: 'http://93.188.164.69:8081',
     // API_BASE_URL: 'http://localhost:8081/api',
     // API_DOC_URL: 'http://localhost:8081',
 };
